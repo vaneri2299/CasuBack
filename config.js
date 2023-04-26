bcd036131c8a8bcb96eead33891e1916026b2011
Ariana Medina <medina2.ariana9@gmail.com>
2023-04-26 18:54:07 -0400

    feat: back

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 const config = {
-  dbUrl:
-    "mongodb+srv://user:1234@cluster0.njf2een.mongodb.net/Casu?retryWrites=true&w=majority",
+  dbUrl: process.env.DB,
   port: process.env.PORT || 8080,
 };
 
-console.log("gfdfsg",process.env.PORT)
+console.log("gfdfsg", process.env.PORT);
 module.exports = config;
